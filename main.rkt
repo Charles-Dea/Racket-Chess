05d5780100575b468732e75e132933bb906aaf7a
Charles Dea <charliedea@proton.me>
2024-11-20 10:57:39 -0600

    made setters

--- main.rkt
+++ main.rkt
@@ -1,9 +1,17 @@
 #lang racket
 (require 2htdp/universe)
 
-(struct Piece [name isWhite])
+(struct Piece [name isWhite sprite])
 (define emptyCell 'null)
-(define boardVector (vector-immutable (Piece "rook" #f) (Piece "knight" #f) (Piece "bishop" #f) (Piece "king" #f) (Piece "king" #f) (Piece "bishop" #f) (Piece "knight" #f) (Piece "rook" #f)))
+(define boardVector
+  (vector-immutable
+    (vector-immutable(Piece
 (struct WS[board])
 (define(set-board ws board)
   (WS board))
+(define(set-name name piece)
+  (Piece name (Piece-isWhite piece) (Piece-sprite piece)))
+(define(set-isWhite isWhite piece)
+  (Piece (Piece-name piece) isWhite (Piece-sprite piece)))
+(define(set-sprite sprite piece)
+  (Piece (Piece-name piece) (Piece-isWhite piece) sprite))
