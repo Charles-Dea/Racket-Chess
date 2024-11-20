5bbfdeeac1916db781768692f843e221065c7ad9
Charles Dea <charliedea@proton.me>
2024-11-20 17:03:23 -0600

    added mousein function

---
+++ input.rkt
@@ -0,0 +1,4 @@
+#lang racket
+(define(mousein ws x y event)
+  (begin(display"click")ws))
+(provide mousein)
--- start.rkt
+++ start.rkt
@@ -2,4 +2,5 @@
 (require 2htdp/universe)
 (require "main.rkt")
 (require"graphics-engine.rkt")
-(big-bang (WS boardVector) (to-draw draw))
+(require"input.rkt")
+(big-bang (WS boardVector) (to-draw draw) (on-mouse mousein))
