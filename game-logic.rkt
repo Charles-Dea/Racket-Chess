ef71b35eac707625984a481cb4a5a6e5b9275917
a2k6 <adrian@panezic.com>
2024-11-22 00:11:05 -0600

    Queens, bishops, rooks, and knights now move as they should

--- game-logic.rkt
+++ game-logic.rkt
@@ -30,17 +30,176 @@
     
   (cond 
     ;Get the place the player wants to move to, then don't update the the worldstate until we have checked if the move is actually possible
-    #|(and (WS-firstClick ws) (move-is-possible ws coord))|#
-    [(and (WS-firstClick ws) (not (eq? 'null selected-piece)) (boolean=? (Piece-isWhite selected-piece) (WS-isWhiteTurn ws))) (WS (WS-board ws) #f #f (WS-isWhiteTurn ws))]
-    [(WS-firstClick ws)  (WS (move-piece (WS-firstCoord ws) coord (WS-board ws)) #f #f (not (WS-isWhiteTurn ws)))]
+    [(and (WS-firstClick ws)  (not (eq? 'null selected-piece))  (boolean=? (Piece-isWhite selected-piece) (WS-isWhiteTurn ws))) (WS (WS-board ws) #f #f (WS-isWhiteTurn ws))]
+    [(and (WS-firstClick ws) (move-is-possible ws coord))  (WS (move-piece (WS-firstCoord ws) coord (WS-board ws)) #f #f (not (WS-isWhiteTurn ws)))]
     [(eq? selected-piece 'null) ws]
     [(boolean=? (Piece-isWhite selected-piece) (WS-isWhiteTurn ws)) (WS (WS-board ws) #t coord (WS-isWhiteTurn ws))]
     [else ws])))
 
+(define (is-up-unobstructed y destCoord board) 
+  (let* 
+  [
+    (x (BCoord-col destCoord))
+    (space (piece-at board (BCoord y x)))
+    (destY (BCoord-row destCoord))
+  ]
+
+  (cond
+  [(= y destY) #t]
+  [(eq? space 'null) (is-up-unobstructed (sub1 y) destCoord board)]
+  [else #f]
+  ))
+  )
+(define (is-down-unobstructed y destCoord board)
+(let* 
+  [
+    (x (BCoord-col destCoord))
+    (space (piece-at board (BCoord y x)))
+    (destY (BCoord-row destCoord))
+  ]
+  (cond
+  [(= y destY) #t]
+  [(eq? space 'null) (is-down-unobstructed (add1 y) destCoord board)]
+  [else #f]
+  )
+  )
+)
+(define (is-right-unobstructed x destCoord board)
+(let* 
+  [
+    (y (BCoord-row destCoord))
+    (space (piece-at board (BCoord y x)))
+    (destX (BCoord-col destCoord))
+  ]
+  (cond
+  [(= x destX) #t]
+  [(eq? space 'null) (is-right-unobstructed (add1 x) destCoord board)]
+  [else #f]
+  )
+  )
+)
+(define (is-left-unobstructed x destCoord board)
+(let* 
+  [
+    (y (BCoord-row destCoord))
+    (space (piece-at board (BCoord y x)))
+    (destX (BCoord-col destCoord))
+  ]
+  (cond
+  [(= x destX) #t]
+  [(eq? space 'null) (is-left-unobstructed (sub1 x) destCoord board)]
+  [else #f]
+  )
+  )
+)
+
+(define (is-ne-unobstructed x y destCoord board)
+(let*
+  [(pc (BCoord y x))]
+  (cond
+    [(and (= (BCoord-col destCoord) x) (= (BCoord-row destCoord) y)) #t]
+    [(eq? (piece-at board pc) 'null) (is-ne-unobstructed (+ x 1) (- y 1) destCoord board)]
+    [else #f])))
+(define (is-se-unobstructed x y destCoord board)
+(let*
+  [(pc (BCoord y x))]
+  (cond
+    [(and (= (BCoord-col destCoord) x) (= (BCoord-row destCoord) y)) #t]
+    [(eq? (piece-at board pc) 'null) (is-se-unobstructed (+ x 1) (+ y 1) destCoord board)]
+    [else #f])))
+(define (is-sw-unobstructed x y destCoord board)
+(let*
+  [(pc (BCoord y x))]
+  (cond
+    [(and (= (BCoord-col destCoord) x) (= (BCoord-row destCoord) y)) #t]
+    [(eq? (piece-at board pc) 'null) (is-sw-unobstructed (- x 1) (+ y 1) destCoord board)]
+    [else #f])))
+(define (is-nw-unobstructed x y destCoord board)
+(let*
+  [(pc (BCoord y x))]
+  (cond
+    [(and (= (BCoord-col destCoord) x) (= (BCoord-row destCoord) y)) #t]
+    [(eq? (piece-at board pc) 'null) (is-nw-unobstructed (- x 1) (- y 1) destCoord board)]
+    [else #f])))
+
+(define (valid-knight-move ws destCoord)
+  (let* 
+  [ (startCoord (WS-firstCoord ws))
+    (firstX (BCoord-col startCoord))
+    (firstY (BCoord-row startCoord))
+    (endX (BCoord-col destCoord))
+    (endY (BCoord-row destCoord))
+    (deltaX (abs (- firstX endX)))
+    (deltaY (abs (- firstY endY)))]
+    
+    (and (= 3 (+ deltaY deltaX)) (not (or (= deltaX 0) (= deltaY 0))))
+    )
+  )
+
+  (define (valid-rook-move ws destCoord)
+  (let* 
+  [ 
+    (board (WS-board ws))
+    (startCoord (WS-firstCoord ws))
+    (firstX (BCoord-col startCoord))
+    (firstY (BCoord-row startCoord))
+    (endX (BCoord-col destCoord))
+    (endY (BCoord-row destCoord))
+    (deltaX (abs (- firstX endX)))
+    (deltaY (abs (- firstY endY)))]
+    (and (or (= deltaX 0) (= deltaY 0)) 
+    (cond 
+    [(and (not (= firstY 0)) (< endY firstY)) (is-up-unobstructed (sub1 firstY) destCoord board)]
+    [(and (not (= firstY 7)) (> endY firstY)) (is-down-unobstructed (add1 firstY) destCoord board)]
+    [(and (not (= firstX 7)) (> endX firstX)) (is-right-unobstructed (add1 firstX) destCoord board)]
+    [(and (not (= firstX 0)) (< endX firstX)) (is-left-unobstructed (sub1 firstX) destCoord board)]
+    [else #f]
+    ))
+    )
+  )
+
+
+  (define (valid-bishop-move ws destCoord)
+  (let* 
+  [ (board (WS-board ws))
+    (startCoord (WS-firstCoord ws))
+    (firstX (BCoord-col startCoord))
+    (firstY (BCoord-row startCoord))
+    (endX (BCoord-col destCoord))
+    (endY (BCoord-row destCoord))
+    (deltaX  (- endX firstX))
+    (deltaY (- endY firstY))
+    (north (< deltaY 0))
+    (south (> deltaY 0))
+    (west (< deltaX 0))
+    (east (> deltaX 0))]
+    (and (= (abs deltaX) (abs deltaY))
+    (cond
+    [(and north east) (is-ne-unobstructed (add1 firstX) (sub1 firstY) destCoord board)]
+    [(and south east) (is-se-unobstructed (add1 firstX) (add1 firstY) destCoord board)]
+    [(and south west) (is-sw-unobstructed (sub1 firstX) (add1 firstY) destCoord board)]
+    [(and north west) (is-nw-unobstructed (sub1 firstX) (sub1 firstY) destCoord board)]
+    ))
+  )
+)
 ;Are you selecting a moveable piece(is it white's turn when you click a white piece) and 
 
-;(define (move-is-possible) (cond \
-;  []))
+(define (move-is-possible ws destCoord) (let* 
+    [
+    (startCoord (WS-firstCoord ws))
+    (piece (piece-at (WS-board ws) startCoord))
+    (piece-name (Piece-name piece))
+    ]
+
+  (cond 
+  [(string=? piece-name "knight") (valid-knight-move ws destCoord)]
+  [(string=? piece-name "rook") (valid-rook-move ws destCoord)]
+  [(string=? piece-name "bishop") (valid-bishop-move ws destCoord)]
+  [(string=? piece-name "queen") (or (valid-rook-move ws destCoord) (valid-bishop-move ws destCoord))]
+  [else #t]
+  
+  )))
+
 ;(define (possible-moves ws coord) )
 ;check if a special move or special scenario is occurring or about to occur(en passant or pawn -> queen or smt)
 ;(define (special-moves ws coord) )
